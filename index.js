9a6ee3ff6f28bb1fe15d91134d41b23347270d8e
ianad389 <ishianand93@gmail.com>
2023-11-25 11:40:03 -0800

    new code

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  console.log(add(1, 2)); // 3
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  console.log(subtract(10, 5)); // 5
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  console.log(multiply(2, 3)); // 6
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  console.log(divide(12, 6)); // 2
